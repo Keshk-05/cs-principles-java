41fb268fe483ac9c2bdbefc16ac3bea295266ba2
Ata Gowani <atagowani@gmail.com>
2017-10-18 13:16:10 -0500

    Add bubble sort

--- algorithms/BubbleSort.java
+++ algorithms/BubbleSort.java
@@ -3,11 +3,57 @@
 
 public class BubbleSort {
 
-  public static int[] sort(int[] nums) {
+  // method for decending order
+  public static int[] sortDesend(int[] nums) {
+    boolean swap;
+    do {
+      swap = false;
+      for(int i = 0; i < nums.length - 1; i++) {
+        if(nums[i] > nums[i + 1]) {
+          swap = true;
+          int valueHolder;
+          valueHolder = nums[i];
+          nums[i] = nums[i + 1];
+          nums[i + 1] = valueHolder;
+        }
+      }
+    } while(swap);
 
+    return nums;
+  }
+
+  public static int[] sortAsend(int[] nums) {
+    boolean swap;
+    do {
+      swap = false;
+      for(int i = 0; i < nums.length - 1; i++) {
+        if(nums[i] < nums[i + 1]) {
+          swap = true;
+          int valueHolder;
+          valueHolder = nums[i];
+          nums[i] = nums[i + 1];
+          nums[i + 1] = valueHolder;
+        }
+      }
+    } while(swap);
+
+    return nums;
   }
 
   public static void main(String[] args){
+    int[] test = {5,4,3,2,1,5,6};
 
+    int[] result = sortDesend(test);
+    System.out.println("Array in desending order:");
+    for (int val : result) {
+      System.out.print(val + " ");
+    }
+    System.out.println("");
+
+    result = sortAsend(test);
+    System.out.println("Array in asending order:");
+    for (int val : result) {
+      System.out.print(val + " ");
+    }
   }
 }
