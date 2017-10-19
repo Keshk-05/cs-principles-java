9a0a9c993722c266b5a19359ed50dee7f44d842c
Ata Gowani <atagowani@gmail.com>
2017-10-19 11:15:50 -0500

    Add insertion sort

--- algorithms/sorting/InsertionSort.java
+++ algorithms/sorting/InsertionSort.java
@@ -3,11 +3,66 @@
 
 public class InsertionSort {
   
-    public static int[] sort(int[] nums) {
   
+  public static int[] sortDesend(int[] nums) {
+    int dividingIndex = 0;
+
+    for (int i = 1; i < nums.length; i++) {
+      boolean swap = true;
+      for (int j = dividingIndex + 1; j > 0 && swap; j--) {
+        if (nums[j] < nums[j - 1]) {
+          swap = true;
+          int holder = nums[j];
+          nums[j] = nums[j - 1];
+          nums[j - 1] = holder;
+        } else {
+          swap = false;
+        }
+      }
+      dividingIndex++;
     }
-  
-    public static void main(String[] args){
-  
-    }
+
+    return nums;
   }
+
+  public static int[] sortAsend(int[] nums) {
+    int dividingIndex = 0;
+
+    for (int i = 1; i < nums.length; i++) {
+      boolean swap = true;
+      for (int j = dividingIndex + 1; j > 0 && swap; j--) {
+        if (nums[j] > nums[j - 1]) {
+          swap = true;
+          int holder = nums[j];
+          nums[j] = nums[j - 1];
+          nums[j - 1] = holder;
+        } else {
+          swap = false;
+        }
+      }
+      dividingIndex++;
+    }
+
+    return nums;
+  }
+
+  public static void main(String[] args){
+    int[] test = {5,4,3,2,1,5,6};
+    
+    int[] result = sortDesend(test);
+    System.out.println("Array in desending order:");
+    for (int val : result) {
+      System.out.print(val + " ");
+    }
+
+    System.out.println("");
+
+    result = sortAsend(test);
+    System.out.println("Array in asending order:");
+    for (int val : result) {
+      System.out.print(val + " ");
+    }
+
+    System.out.println("");
+  }
+}
