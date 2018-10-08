4612c042da31415209e45a762fd401f2216921e2
Ata Gowani <atagowani@gmail.com>
2018-10-08 00:44:40 -0500

    Add test for LinkedList

--- Main.java
+++ Main.java
@@ -10,5 +10,24 @@ public class Main {
     System.out.println("Node value: " + n.getValue());
     System.out.println("Node key: " + n.getKey());
     /** **/
+
+    /** LinkedList Data Structure **/
+    LinkedList l = new LinkedList();
+    for (int i = 0; i < 10; i++) {
+      String s = "Item " + i;
+      Node a;
+      if (i == 5)
+        a = new Node(s, 1);
+      else
+        a = new Node(s, i);
+      if (l.add(a)) {
+        System.out.println("Node with key " + a.getKey() + " added successfully!");
+      } else {
+        System.out.println("Unable to add node with key " + a.getKey());
+      }
+    }
+
+    l.print();
+    /** **/
   }
 }
